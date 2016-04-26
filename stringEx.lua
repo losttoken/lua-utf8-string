a257796cd7ac6d13c191c47e1fafb73040bf02d3
sunpeng <peng.sun@emagroup.cn>
2016-04-26 15:11:58 +0800

    update string.usub

--- stringEx.lua
+++ stringEx.lua
@@ -9,12 +9,17 @@ string.ulist=function(str)
     return list
 end
 
-string.usub = function(str,sum)
+string.usub = function(str,i,j)
     local list=string.ulist(str)
     local substr=""
-    for j=1,sum,1 do
-        if list[j]~=nil then
-            substr=substr..list[j]
+	if i==0 then i=1 end
+	if j==0 then j=1 end
+	i=i>0 and i or #list+i
+	j=j>0 and j or #list+j
+	if i>j then i=i+j;j=i-j;i=i-j; end	
+    for k=i,j,1 do
+        if list[k]~=nil then
+            substr=substr..list[k]
         end
     end
     return substr
